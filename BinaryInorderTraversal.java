7e0b7b96ce2e60ee275d778ecfa728194df60147
dhananjayjakhadi <31828887+dhananjayjakhadi@users.noreply.github.com>
2019-10-29 21:16:59 -0700

    Leetcode #94 Binary Tree order traversal

--- BinaryInorderTraversal.java
+++ BinaryInorderTraversal.java
@@ -3,13 +3,12 @@ class BinaryInorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root){
         ArrayList<Integer> result = new ArrayList<Integer>();
         Stack<TreeNode> s = new Stack<TreeNode>();
-
         TreeNode p = root;
+       
         while(p != null){
             s.push(p);
             p = p.left;
         }
-        
         while(!s.isEmpty()){
             TreeNode t = s.pop();
             result.add(t.val);
