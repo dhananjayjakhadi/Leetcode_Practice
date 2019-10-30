f80bd67424bcca37e808c9beb7db23f8c5281217
dhananjayjakhadi <31828887+dhananjayjakhadi@users.noreply.github.com>
2019-10-29 19:00:32 -0700

    Leetcode #206 Reverse a Linked list

---
+++ ReverseLinkedList.java
@@ -0,0 +1,15 @@
+public class ReverseLinkedList {
+    public ListNode reverseLinkedList(ListNode head){
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
